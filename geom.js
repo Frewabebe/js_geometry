54b5a8f52200566aa62056bc04e28c85e64cd870
Frew A Negash <zulufanke@yahoo.com>
2017-07-08 20:50:01 -0400

    worked on geometric problems

--- geom.js
+++ geom.js
@@ -3,6 +3,20 @@ class Rectangle {
     this.length = length;
     this.width = width;
   }
+    isSquare(){
+      if(this.length === this.width){
+        return true
+      }
+      else {
+        return false
+      }
+    }
+    area(){
+      return this.length * this.width
+    }
+    perimeter(){
+      return (this.length + this.width)*2
+    }
 }
 
 
@@ -12,6 +26,35 @@ class Triangle {
     this.sideB = sideB;
     this.sideC = sideC;
   }
+  isEquilateral(){
+    if(this.sideA === this.sideB && this.sideA=== this.sideC){
+    return true
+  }
+  else {
+    return false
+  }
+}
+ isIsosceles(){
+   if(this.sideA === this.sideB || this.sideA === this.sideC || this.sideB === this.sideC){
+     return true
+   }
+   else{
+     return false
+   }
+ }
+ isObtuse(){
+   if(this.sideA !== this.sideB && this.sideA!== this.sideC){
+   return true
+ }
+ else {
+   return false
+ }
+}
+area(){
+  let p = (this.sideA + this.sideB + this.sideC)/2
+  let myArea = Math.sqrt(p * (p - this.sideA) * (p - this.sideB) * (p - this.sideC))
+  return myArea
+}
 }
 
 
@@ -22,7 +65,15 @@ class LineSegment {
     this.x2 = x2;
     this.y2 = y2;
   }
+
+  length (){
+    let ab = Math.sqrt(Math.pow(this.x1 - this.x2) + Math.pow(this.y1 - this.y2))
+    return (ab)
+  }
 }
+// need to try again..
+
+
 
 // NOTE: DO NOT REMOVE OR ALTER
 module.exports = {
